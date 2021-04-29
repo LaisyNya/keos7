8bab42d8d64b22c3b56aa2166aa2d5f55d6e2dee
LaisyNya <82419550+LaisyNya@users.noreply.github.com>
2021-04-29 10:26:34 -0700

    Update education.js

--- database/menu/education.js
+++ database/menu/education.js
@@ -12,13 +12,5 @@ const education = (prefix, botName, ownerName) => {
   ├─ ❏ ${prefix}chatlist
   ├─ ❏ ${prefix}ping
   └─ ❏ ${prefix}bugreport
-◪ *EDUCATION*
-  │
-  ├─ ❏ ${prefix}wiki
-  ├─ ❏ ${prefix}wikien
-  ├─ ❏ ${prefix}nulis
-  ├─ ❏ ${prefix}quotes
-  ├─ ❏ ${prefix}quotes2
-  └─ ❏ ${prefix}artinama`
 }
 exports.education = education
