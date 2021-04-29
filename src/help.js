c4218cc0a78bcb480ec8123462d37ba3110bca94
LaisyNya <82419550+LaisyNya@users.noreply.github.com>
2021-04-29 10:49:01 -0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -4,10 +4,7 @@ const help = (prefix) => {
 
 ◪ *Información*
   ❏ Comando: 「  ${prefix}  」
-  ❏ Creador: Shanduy
-  ❏ Como instalar el bot: https://youtu.be/rfxuYsWGmQo
-  ❏ Instagram: https://www.instagram.com/thepavos
-
+  ❏ Creador: LAISY SODA
 ◪ *Para Usar Al Bot*
   ❏ Registrate con el comando ${prefix}daftar Nombre
 
@@ -30,11 +27,6 @@ const help = (prefix) => {
   ├─ De sticker a JPG
   ├─ ❏ ${prefix}tomp3
   └─ De video a MP3
-
-◪ *DESCARGAS*
-  │
-  ├─ ❏ ${prefix}ytmp4
-  └─ Mas link del video de YT
 
 ◪ *AUDIO*
   │
@@ -62,9 +54,6 @@ const help = (prefix) => {
   ├─ ❏ ${prefix}promote
   ├─ Dar admin a un miembro
   ├─ ❏ ${prefix}demote
-  ├─ Quitar el admin
-  ├─ ❏ ${prefix}linkgc
-  ├─ Link del grupo
   ├─ ❏ ${prefix}tagall
   └─ Nombra a todos los del grupo
 
@@ -76,12 +65,8 @@ Para usar estas funciones el bot necesita admin
 
 Para activar los NSFW coloque el siguiente comando ${prefix}nsfw 1 y para desactivar los NSFW coloque ${prefix}nsfw 0
 
-No te olvides de seguirme en instagram flaco :)
 
-Para saber la ultima version mas actual del bot entra en 
-https://github.com/shanduy/ShanBot
-
-by Shanduy
+by LAISY
 `
 }
 
